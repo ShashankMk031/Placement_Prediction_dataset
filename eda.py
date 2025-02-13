606334ba58de8f4f81c52782a25b8f556456a648
Shashank <shashankmk031@gmail.com>
2025-02-13 14:53:20 +0530

    Initial commit

--- eda.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd 
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-#read data 
-
-data = pd.read_csv("E:\Placement predictor dataset\placementdata.csv")
-data.head()
